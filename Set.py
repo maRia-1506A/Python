ad44df1f681b184d792a6530bb83256316d3f63b
maRia-1506A <zannatulfarzana363@gmail.com>
2025-05-08 11:09:01 +0600

    Added two new file

--- Set.py
+++ Set.py
@@ -12,3 +12,51 @@ for i in set1:  # for all item
     print(i)
 
 print('9 is in the list:', 9 in set1)  # specific item
+print('')
+
+
+# add set items
+set2 = {'m', 'a', 'r', 'i', 'a'}
+set2.add('A')
+print(set2)
+
+# update item
+tuple2 = (1, 2, 3, 4, 5)
+set2.update(tuple2)
+print("Updated set", set2)
+print(type(set2))
+print('')
+
+
+# remove set items
+set3 = {10, 20, 40, 30, 50, 70}
+print(set3)
+set3.remove(70)
+print(set3)
+
+set3.discard(100)  # discard method
+print(set3)
+
+set3.clear()
+print(set3)
+print('')
+
+
+# loop set items
+set4 = {"maria", "afridi", "mishka", "aziz", "efath"}
+for i in set4:
+    print(i)
+print('')
+
+
+# join set items
+set5 = {"a", "b", "c"}
+set6 = {1, 2, 3}
+print(set5.union(set6))  # can add multiple set
+
+set6.update(set5)  # update method
+print(set6)
+
+# join multiple sets
+myset = set4 | set5 | set6
+print("Myset:",myset)
--- StringFormat.py
+++ StringFormat.py
@@ -1,16 +1,31 @@
-n1= 10
-n2= 20
-name="maria"
-id= 106
+n1 = 10
+n2 = 20
+name = "maria"
+id = 106
 print(f"My name is {name} and my id is {id}")
-print(f"Calculation by formatting: {n1+n2}")
+print(f"Calculation by formatting: {n1+n2:.2f}")
+print('')
+
 
 # single line comment
 print("Hello world")
 print(420)
 ''' multi line 
 comment'''
-name= "maria"
-id1=106
-id2= 364.0
+name = "maria"
+id1 = 106
+id2 = 364.0
 print(name, id1, id2)
+print('')
+
+
+# upper & lower case
+s = "Hello World"
+print(s.upper())
+print(s.lower())
+
+# remove space at begining
+m = " Maria"
+print(m)
+print(m.strip())
+print("After replacing:",m.replace("M", "N"))
