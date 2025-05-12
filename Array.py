11c8b1b417d07a907d063ffbeec8074d9b7d0b41
maRia-1506A <zannatulfarzana363@gmail.com>
2025-05-12 10:47:39 +0600

    Add 2 files

---
+++ Array.py
@@ -0,0 +1,3 @@
+Array = ['a', 's', 'd', 'f', 'g']
+Array[0] = 'm'
+print(Array)
---
+++ ClassObj.py
@@ -0,0 +1,19 @@
+class NewClass1():  # class
+    name = ''  # property
+    number = ''
+    id = ''
+    section = ''
+
+
+a = NewClass1()  # object
+b = NewClass1()  # object
+a.name = 'maria'
+a.id = 106
+
+b.name = 'afridi'
+b.number= '01608233419'
+
+print(a.name)
+print(a.id)
+print(b.name)
+print(b.number)
