d2497259686a83fd39cc115eb729d40ebedd849a
Zannatul Farzana Maria <zannatulfarzana363@gmail.com>
2025-05-21 22:09:57 +0600

    Add files via upload

--- StringFormat.py
+++ StringFormat.py
@@ -3,8 +3,7 @@ n2 = 20
 name = "maria"
 id = 106
 print(f"My name is {name} and my id is {id}")
-print(f"Calculation by formatting: {n1+n2:.2f}")
-print('')
+print(f"Calculation by formatting: {n1+n2:.2f}\n")
 
 
 # single line comment
@@ -15,8 +14,7 @@ comment'''
 name = "maria"
 id1 = 106
 id2 = 364.0
-print(name, id1, id2)
-print('')
+print(name, id1, id2, "\n")
 
 
 # upper & lower case
@@ -28,4 +26,25 @@ print(s.lower())
 m = " Maria"
 print(m)
 print(m.strip())
-print("After replacing:",m.replace("M", "N"))
+print("After replacing:", m.replace("M", "N"), "\n")
+
+
+# string slicing
+name = "Maria"
+slicing = name[0:3]  # start from 0 till 3 [excluding 3]
+print(slicing)
+print(name[-4:-1])  # same as [1:4]
+print(name[:4])
+print(name[1:])  # same as [1:5]
+
+# string slicing with skip value
+a = "0123456789"
+print(a[1:8])
+print(a[1:8:2], "\n")
+
+
+# String function
+name = "zannatul"
+print(len(name))
+print(name.endswith("tul"))
+print(name.capitalize())
