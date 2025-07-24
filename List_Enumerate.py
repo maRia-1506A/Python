bb6cefe03629edb4bb38c79ce9efeed3c813bcfc
maRia-1506A <zannatulfarzana363@gmail.com>
2025-07-24 21:43:01 +0600

    nothing

--- List_Enumerate.py
+++ List_Enumerate.py
@@ -6,5 +6,6 @@ l = [10, 20, 30, 40]
 #     index += 1
 
 # this can be simplified using enumerate function
-for index, item in enumerate(l):
+# enumerate(iterable, start)
+for index, item in enumerate(l, start=1):
     print(f"The item number at index {index} is {item}")
